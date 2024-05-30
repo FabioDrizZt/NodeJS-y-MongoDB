2258f635cb06f763d689b57f8f2ea309b3f63754
fabio <fabio>
2024-05-30 19:41:56 -0300

    configuración de conexión/desconexión de mongodb

--- readme.md
+++ readme.md
@@ -5,3 +5,8 @@ npm install express mongodb
 crear un archivo en src/mongodb.js donde pondremos la configuración de conexión a la BD
 creamos el archivo server.js que será el punto de entrada a nuestra aplicación
 crear un archivo .env donde irá nuestras credenciales/ruta de conexión
+
+configurar nuestro archivo mongo:
+
+- Cargamos el archivo .env process.loadEnvFile()
+- traemos el cliente: const { MongoClient } = require('mongodb')
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,2 +1,29 @@
-process.loadEnvFile()
+process.loadEnvFile() // Cargamos las variables de entorno desde el archivo .env
 const { MongoClient } = require('mongodb')
+
+//Obtenemos la URI desde las variables de entorno
+const URI = process.env.MONGODB_URLSTRING
+//Hacemos la conexión del cliente
+const client = new MongoClient(URI)
+
+async function connectToMongoDB() {
+  try {
+    await client.connect()
+    console.log('Conectandose a mongoDB')
+    return client
+  } catch (error) {
+    console.error('Error al conectar a MongoDB')
+    return null
+  }
+}
+
+async function disconnectFromMongoDB() {
+  try {
+    await client.close()
+    console.log('Desconectandose de mongoDB')
+  } catch (error) {
+    console.error('Error al desconectar de MongoDB')
+  }
+}
+
+module.exports = { connectToMongoDB, disconnectFromMongoDB }
